5223c1a9b992451a72392db3f988d6b5f5194ce4
Tyler Roach <tyler.roach@sparcedge.com>
2014-08-23 20:34:17 -0400

    switch key + final build

--- app/src/main/java/app/leftovers/hackathon/sparc/com/leftovers/Constants.java
+++ app/src/main/java/app/leftovers/hackathon/sparc/com/leftovers/Constants.java
@@ -4,7 +4,7 @@ package app.leftovers.hackathon.sparc.com.leftovers;
  * Created by tylerroach on 8/23/14.
  */
 public class Constants {
-    public static final String API_KEY = "27749ad89c85c9a7d74a51abad36cd96";
+    public static final String API_KEY = "0051927f71d94e11d2dcdda167b4559c";
     public static final String API_SEARCH = "http://food2fork.com/api/search?key=";
     public static final String API_GET = "http://food2fork.com/api/get?key=";
 }
